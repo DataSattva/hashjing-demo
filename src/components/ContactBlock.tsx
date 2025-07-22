ed59a3cf3a25a7394e5fb7f9c16d93cb0712f64e
Alexander <02481632@proton.me>
2025-07-22 11:07:40 +0300

    chore(footer): add reference to external contacts and resources index

--- src/components/ContactBlock.tsx
+++ src/components/ContactBlock.tsx
@@ -1,117 +1,25 @@
 // src/components/ContactBlock.tsx
 import { useEffect, useState } from 'react'
 
-/* ---------- JSON type helpers ---------- */
-type Email   = { email: string;   caption: string; description: string };
-type XHandle = { x: string;       caption: string; description: string };
-type Discord = { discord: string; caption: string; description: string };
-
-type Contacts = {
-  emails:  Email[];
-  x:       XHandle[];
-  discord: Discord[];
-};
-
-/* each resource object has 1 dynamic URL key + caption + description */
-type ResourceItem = {
-  caption: string;
-  description: string;
-  /* [urlKey: string]: string */
-  [k: string]: string;
-};
-
-type Resources = { HJ: ResourceItem[] };
-
-type JsonData = {
-  contacts: Contacts;
-  resources: Resources;
-};
-
 export const ContactBlock = () => {
-  const [data, setData] = useState<JsonData | null>(null);
-
-  useEffect(() => {
-    fetch('https://datasattva.github.io/hashjing-res/res.json')
-    //fetch("./public/res.json")
-      .then((res) => res.json())
-      .then(setData)
-      .catch((err) => console.error("Error loading res.json:", err));
-  }, []);
-
-  if (!data) return <p className="status">Loading contacts & resources…</p>;
-
-  const { contacts, resources } = data;
 
   return (
     <>
       {/* ------------- CONTACTS ------------- */}
       <section id="contacts">
-        <h2 className="section-title">Contacts</h2>
+        <h2 className="section-title">Contacts and resources</h2>
         <div id="contacts-content">
-          {/* E-mails */}
-          {contacts.emails.map(({ email, caption, description }) => (
-            <p key={email}>
-              <strong>{caption}: </strong>
-              <a href={`mailto:${email}`}>{email}</a>
-              <br />
-              {description}
-            </p>
-          ))}
-
-          {/* Discord entries */}
-          {contacts.discord.map(({ discord, caption, description }) => (
-            <p key={discord}>
-              <strong>{caption}: </strong>
-              <a
-                href={`${discord.replace("@", "")}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {discord}
-              </a>{" "}
-              <br />
-              {description}
-            </p>
-          ))}
-
-          {/* X / Twitter list */}
-          {contacts.x.map(({ x, caption, description }) => (
-            <p key={x}>
-              <strong>{caption}: </strong>
-              <a
-                  href={`https://x.com/${x.replace("@", "")}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {x}
-                </a>{" "}
-              <br />
-              {description}
-            </p>
-          ))}
-        </div>
-      </section>
-
-      {/* ------------- RESOURCES ------------- */}
-      <section id="resources">
-        <h2 className="section-title">Resources</h2>
-        <div id="contacts-content">
-          {resources.HJ.map((item) => {
-            const urlKey = Object.keys(item).find(
-              (k) => k !== "caption" && k !== "description"
-            ) as string;
-            const url = item[urlKey];
-            return (
-              <p key={urlKey}>
-                <strong>{item.caption}: </strong>
-                <a href={url} target="_blank" rel="noreferrer">
-                  {url}
-                </a>
-                <br />
-                {item.description}
-              </p>
-            );
-          })}
+          <p>
+            For a detailed list of HashJing contacts and resources, see the page&nbsp;
+            <a
+              href="https://datasattva.github.io/hashjing-res/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Contacts and Resources
+            </a>
+            .
+          </p>
         </div>
       </section>
     </>
